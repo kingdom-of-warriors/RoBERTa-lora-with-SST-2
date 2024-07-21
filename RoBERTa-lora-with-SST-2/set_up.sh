1dcecaadb0cb4d25ac90481f2b2c23162c8fb8e4
ljr <3179423949@qq.com>
2024-07-21 19:07:41 +0800

    加入readme文件

--- RoBERTa-lora-with-SST-2/set_up.sh
+++ RoBERTa-lora-with-SST-2/set_up.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+read -p "请输入要创建的 Conda 环境名称: " env_name
 
 # 创建并激活 Conda 环境
-conda create -n test python=3.9 -y
-source activate test
+conda create -n $env_name python=3.9 -y
+source activate $env_name
 
 # 安装 transformers 库
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple transformers==4.39.2
