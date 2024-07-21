1912b077ed3b9e05be3ffe74911c74dfc56d0d6b
ljr <3179423949@qq.com>
2024-07-21 18:37:32 +0800

    删除：     RoBERTa-lora-with-SST-2/requiements.txt 	修改：     RoBERTa-lora-with-SST-2/set_up.sh

--- RoBERTa-lora-with-SST-2/set_up.sh
+++ RoBERTa-lora-with-SST-2/set_up.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-# Create conda environment
-conda create -n AI python=3.9 -y
-conda activate AI
+# 创建并激活 Conda 环境
+conda create -n test python=3.9 -y
+source activate test
 
-# Install CUDA toolkit
+# 安装 transformers 库
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple transformers==4.39.2
+
+# 安装 cudatoolkit
 conda install cudatoolkit==11.8 -y
 
-# Install Python packages
-pip install -r requirements.txt
+# 安装 PyTorch 及其相关库
+pip3 install numpy --pre torch torchvision torchaudio --force-reinstall --index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+# 安装 auto-gptq
+pip install auto-gptq --no-build-isolation
+
+# 安装其他常用库
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple ipykernel evaluate tqdm scikit-learn
+
+echo "All installations are complete."
